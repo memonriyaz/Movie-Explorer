0d57a5a50d9a3d7212613e703d58a2d559d7b0c8
Riyaz Memon <memonriyazwork@gmail.com>
2025-07-04 11:14:29 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    
     extend: {
       colors: {
         background: "var(--background)",
@@ -14,6 +15,7 @@ const config: Config = {
       },
     },
   },
+  darkMode: "class",
   plugins: [],
 };
 export default config;
